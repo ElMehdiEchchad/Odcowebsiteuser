593bcc5e2ba08e444ac0061433f69b7d566d77f6
ElMehdiEchchad <58845064+ElMehdiEchchad@users.noreply.github.com>
2020-05-17 14:49:19 +0000

    Update db_connect.php

--- SiteODCOfinal/includes/db_connect.php
+++ SiteODCOfinal/includes/db_connect.php
@@ -1,6 +1,6 @@
 <?php
   try{
-    $db=new PDO('mysql:host=pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=	dduqnfxjfazikpup','x56nty91zd1ta89f','buxoeqihlctss731');
+    $db=new PDO('mysql:host=pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=dduqnfxjfazikpup','x56nty91zd1ta89f','buxoeqihlctss731');
 
   }
   catch(PDOException $e){
